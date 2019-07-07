8ffb2b3215fc726f814b92d5701141aa09124c22
Uros Randelovic <uros.randelovic@gmail.com>
2019-07-07 02:14:24 -0400

    first commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI: "mongodb://mern-auth:jana1997@ds347917.mlab.com:47917/mern-auth" 
+  };
+
+module.exports = {
+  mongoURI: "mongodb://mern-auth:jana1997@ds347917.mlab.com:47917/mern-auth",
+  secretOrKey: "secret"
+};
